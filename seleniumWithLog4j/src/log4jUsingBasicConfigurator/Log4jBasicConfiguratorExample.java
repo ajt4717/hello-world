4b9475b3e30eed10e47c9a0d8ecff6df663cd51c
Ajt <ajt4717@gmail.com>
2017-11-08 01:52:23 +0530

    vvv

--- seleniumWithLog4j/src/log4jUsingBasicConfigurator/Log4jBasicConfiguratorExample.java
+++ seleniumWithLog4j/src/log4jUsingBasicConfigurator/Log4jBasicConfiguratorExample.java
@@ -19,7 +19,7 @@ public class Log4jBasicConfiguratorExample {
 		//BasicConfigurator.configure();
 		
 		Logger log1=Logger.getLogger(Log4jBasicConfiguratorExample.class);
-		System.out.println("*****");
+		System.out.println("****");
 		BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%d{HH:mm} %-5p %F %50.50c - %m%n")));
 		
 		
